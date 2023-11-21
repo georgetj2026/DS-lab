8e99d6170a260fd2b06d126ef9aa37ca8f865637
jo <145546435+georgetj2026@users.noreply.github.com>
2023-11-21 16:02:00 +0530

    Update 13tree.c

--- 13tree.c
+++ 13tree.c
@@ -1,25 +1,34 @@
 #include<stdio.h>
 void main() {
-int i,ch,n;
+int i,pos,n;
+char CH='y';
 printf("enter the no of nodes\n");
 scanf("%d",&n);
 int a[n];
 printf("enter the numbers of tree elements\n");
 for(i=1;i<=n;i++) 
 {
-  printf("enter the %d element\n",i);
+  printf("enter the %d element :\t",i);
     scanf("%d",&a[i]);
     }
-  printf("enter the node to search\n");
-  scanf("%d",&ch);
-  if(ch<2) 
-   printf("%d has no parent\n",a[ch]);
+  while(CH=='y') {
+  printf("enter the node to search\n");   
+  scanf("%d",&pos);
+  if(pos<2) 
+   printf("%d has no parent\n",a[pos]);
   else
-  printf("parent of %d = %d\n",a[ch],a[ch/2]);
-  if(ch*2>n)
-       printf("%d has no leftchild\n",a[ch]);
+  printf("parent of %d = %d\n",a[pos],a[pos/2]);
+  if(pos*2>n)
+       printf("%d has no leftchild\n",a[pos]);
   else
-  printf("left child of %d =%d\n",a[ch],a[ch*2]);
+  printf("left child of %d =%d\n",a[pos],a[pos*2]);
+  if((pos*2)+1>n)
+   printf("%d has no rightchild\n",a[pos]);
+   else
+  printf("right child of %d =%d\n",a[pos],a[(pos*2)+1]);
+printf("do you want to continue y/n:\t");
+scanf("%s",&CH);
+}},a[ch*2]);
   if((ch*2)+1>n)
    printf("%d has no rightchild\n",a[ch]);
    else
